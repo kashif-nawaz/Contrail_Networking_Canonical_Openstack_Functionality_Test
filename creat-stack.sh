18bfd6def541d767ad03cf7e56d9a737e7b74571
knawaz <knawaz@juniper.net>
2020-06-16 14:50:55 +0000

    dpdk flavor added

--- creat-stack.sh
+++ creat-stack.sh
@@ -3,7 +3,10 @@
 source admin-openrc.sh.txt 
 openstack stack create -t glance_add_image.yml image
 sleep 30
+
+#Plesae refer dpdk flavor in *vn_vm*.yml files if an instance is required to be created on DPDK node
 openstack stack create -t add_flavor.yml flavor
+openstack stack create -t add_flavor_dpdk.yml dpdk_flavor
 sleep 5
 openstack stack create -t security_group_add.yml sec_group
 sleep 5
@@ -13,6 +16,7 @@ openstack stack create -t network-policy.yml test-network-policy
 openstack stack create -t left_virtual_network_add.yml left_vn
 openstack stack create -t right_virtual_network_add.yml right_vn
 sleep 5
+#Please change az as per your environment 
 #If more VMs are required on a VN then copy left_vn_vm_1_add.yml or right_vn_vm_1_add.yml and ammend it accordingly
 #Once new file is ready it should be listed below 
 openstack stack create -t left_vn_vm_1_add.yml left_vm_1
--- delete-stack.sh
+++ delete-stack.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 source admin-openrc.sh.txt
+#stack should be deleted in reverse order of stack.sh scripit
+#Please ammend this file as per your enviornment
 openstack stack delete left_vm_1 -y --wait
 openstack stack delete right_vm_1 -y --wait
 sleep 5
@@ -12,5 +14,6 @@ sleep 5
 openstack stack delete sec_group -y --wait
 sleep 5
 openstack stack delete flavor -y --wait
+openstack stack delete dpdk_flavor -y --wait
 sleep 5
 openstack stack delete image -y --wait
